2f91ca267ab84aeac205bfb69ece2a229f23c6d4
rbsaulodev <rb.saulodev@gmail.com>
2025-10-01 15:03:37 -0300

    fix: Corrige aninhamento do repositório e adiciona arquivos do projeto

---
+++ primeiro-app-android/app/src/main/java/com/rafael/firstapplication_2/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.rafael.firstapplication_2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rafael.firstapplication_2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // val button = findViewById<Button>(R.id.button)
+        // val editText = findViewById<EditText>(R.id.edit_text)
+        val editText = binding.editText
+        val button = binding.button
+
+        button.setOnClickListener { view ->
+            println(editText.text)
+        }
+    }
+}
